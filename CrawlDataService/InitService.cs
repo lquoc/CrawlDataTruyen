e4d191061330dfee34f8968f567a028e30fe324c
quoc08510 <quoc08510@gmail.com>
2024-08-25 20:21:34 +0700

    update service

--- CrawlDataService/InitService.cs
+++ CrawlDataService/InitService.cs
@@ -1,10 +1,10 @@
-﻿using Amazon.Polly;
-using Amazon;
+﻿using Amazon;
+using Amazon.Polly;
+using Amazon.Runtime;
 using Common;
+using CrawlDataService.Common;
 using CrawlDataService.Service;
 using Microsoft.Extensions.DependencyInjection;
-using Amazon.Runtime;
-using CrawlDataService.Common;
 
 namespace CrawlDataService
 {
@@ -14,12 +14,28 @@ namespace CrawlDataService
         {
             var service = new ServiceCollection();
             AddServiceCore(service);
+            AddServiceCrawl(service);
             IServiceProvider serviceProvider = service.BuildServiceProvider();
             RuntimeContext._serviceProvider = serviceProvider;
         }
+        private static void AddServiceCrawl(IServiceCollection services)
+        {
+            var types = typeof(CrawlNovelFromWiki).Assembly.GetTypes().ToList();
+            var typesServiceCrawl = types.Where(e => e.IsSubclassOf(typeof(CrawlNovelSerivce)));
+            foreach (var type in typesServiceCrawl)
+            {
+                services.AddScoped(type);
+                var interfaces = type.GetInterfaces();
+                foreach (var i in interfaces)
+                {
+                    services.AddScoped(i, type);
+                }
+            }
+        }
+
+
         private static IServiceCollection AddServiceCore(ServiceCollection service)
         {
-            service.AddScoped<CrawlNovelFromWiki>();
             service.AddSingleton<IAmazonPolly>(sp =>
             {
                 var config = new AmazonPollyConfig
@@ -31,7 +47,6 @@ namespace CrawlDataService
             });
             service.AddScoped<ChangeTextToVoice>();
             service.AddScoped<MP4Service>();
-            service.AddScoped<CrawlNovelFromDTruyen>();
             service.AddScoped<ManagerService>();
             return service;
         }
